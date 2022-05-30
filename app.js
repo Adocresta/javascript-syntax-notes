fd11da279acaad0a5ef491bdd8ac1c1e640e0651
Onur <znygaga004@gmail.com>
2022-05-30 18:52:12 +0300

    learned about js arrays and switch statement

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ if (1 == "1") {
   console.log("this will be printed");
 }
 
+// indexof returns us -1 if means no space
 let password = prompt("Enter your password: ");
 console.log(password.indexOf(" "));
 
@@ -15,3 +16,45 @@ if (password.length >= 6 && password.indexOf(" ") < 0) {
 } else {
   console.log("invalid password");
 }
+
+// && runs before || all the time unless paranthesis
+
+// **switch statement
+
+let dayPast = 0;
+// first day ends at 24:00
+dayPast++;
+
+switch (dayPast) {
+  case 1:
+    console.log("You've survived the first day Congratulations");
+    break;
+  case 2:
+    console.log("You've survived the Second day Congratulations");
+  // if you want same outcome leave it empty
+  case 3:
+  case 4:
+    console.log("You've survived the 3rd and 4th day Congratulations");
+  default:
+    break;
+}
+
+// defining empty array
+
+let colors = [];
+
+//can't change string values
+
+// indexOf checks if element exist if not give -1
+
+// we use  const with array because reference
+
+const airplaneSeats = [
+  ["Ruth", "Anthony", "Stevie"],
+  ["Amelia", "Pedro", "Maya"],
+  ["Xavier", "Ananya", "Luis"],
+  ["Luke", null, "Deniz"],
+  ["Rin", "Sakura", "Francisco"],
+];
+
+console.log(airplaneSeats);
---
+++ array_cheatsheet.js
@@ -0,0 +1,6 @@
+// Important array methods:
+//push(value) - adds value to the END of an array
+//pop() - removes and returns last value in array
+
+//unshift(val) - adds value to START of an array
+//shift() - removes and returns first element in an array
